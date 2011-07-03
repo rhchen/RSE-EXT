5993c491ec7f8e9d2dc194b8dd4737700e820802
rhchen <ranhow.chen@gmail.com>
2011-07-03 16:48:44 +0800

    Add feature build
    	modified:   releng/plugins/net.izhtw.rse.releng/build.properties
    	modified:   releng/plugins/net.izhtw.rse.releng/build.sh

--- releng/plugins/net.izhtw.rse.releng/build.sh
+++ releng/plugins/net.izhtw.rse.releng/build.sh
@@ -2,6 +2,7 @@
 
 eclipseDist="/home/rhchen/Downloads/eclipse/eclipse/eclipse-SDK-3.7-linux-gtk.tar.gz"
 cdtDist="/home/rhchen/Downloads/eclipse/cdt/cdt-master-8.0.0-I201106081058.zip"
+rseDist="/home/rhchen/Downloads/eclipse/rse/RSE-SDK-3.3.zip"
 
 workDir="/home/rhchen/works/build/rse"
 rm -Rf ${workDir}
@@ -32,6 +33,14 @@ git clone git://github.com/rhchen/RSE-EXT.git
 
 cd ${baseLocation}
 tar -xzvf ${eclipseDist}
+
 unzip -o -d cdt ${cdtDist}
 cp -Rf cdt/plugins/* eclipse/plugins/
 cp -Rf cdt/features/* eclipse/features/
+rm -Rf cdt
+
+unzip -o -d rse ${rseDist}
+cp -Rf rse/eclipse/plugins/* eclipse/plugins/
+cp -Rf rse/eclipse/features/* eclipse/features/
+rm -Rf rse
+
