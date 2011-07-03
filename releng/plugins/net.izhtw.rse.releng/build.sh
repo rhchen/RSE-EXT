0231ba24e257c9b1289b44a11334ffc93f263946
rhchen <ranhow.chen@gmail.com>
2011-07-03 15:27:49 +0800

    Add build.properties in releng
    	new file:   releng/plugins/net.izhtw.rse.releng/build.properties
    	modified:   releng/plugins/net.izhtw.rse.releng/build.sh

--- releng/plugins/net.izhtw.rse.releng/build.sh
+++ releng/plugins/net.izhtw.rse.releng/build.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
-workdir="/home/rhchen/works/build/rse"
-rm -Rf ${workdir}
-mkdir -p ${workdir}
-echo "workdir is ${workdir}"
+eclipseDist="/home/rhchen/Downloads/eclipse/eclipse/eclipse-SDK-3.7-linux-gtk.tar.gz"
+cdtDist="/home/rhchen/Downloads/eclipse/cdt/cdt-master-8.0.0-I201106081058.zip"
 
-srcdir=${workdir}/src
-mkdir -p ${srcdir}
-echo "srcdir is ${srcdir}"
+workDir="/home/rhchen/works/build/rse"
+rm -Rf ${workDir}
+mkdir -p ${workDir}
+echo "workdir is ${workDir}"
+
+buildDirectory=${workDir}/buildDirectory
+mkdir -p ${buildDirectory}
+echo "buildDirectory is ${buildDirectory}"
+
+baseLocation=${workDir}/baseLocation
+mkdir -p ${baseLocation}
+echo "baseLocation is ${baseLocation}"
+
+buildConfiguration=${workDir}/buildConfiguration
+mkdir -p ${buildConfiguration}
+echo "buildConfiguration is ${buildConfiguration}"
+
+srcDir=${workDir}/src
+mkdir -p ${srcDir}
+echo "srcdir is ${srcDir}"
 
 # Checkout source codes
 echo "Fetch Source Codes from github ....."
 
-cd ${srcdir}
-
+cd ${srcDir}
 git clone git://github.com/rhchen/RSE-EXT.git
 
+cd ${baseLocation}
+tar -xzvf ${eclipseDist}
+unzip -o -d cdt ${cdtDist}
+cp -Rf cdt/plugins/* eclipse/plugins/
+cp -Rf cdt/features/* eclipse/features/
