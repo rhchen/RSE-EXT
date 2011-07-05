6811d10c8b8c5b91235dfb376102fcd33b78c068
rhchen <ranhow.chen@gmail.com>
2011-07-05 23:50:53 +0800

    A try of Connector Service
    	modified:   rse/plugins/net.izhtw.rse.subsystems.processes.emulator.local/build.properties
    	new file:   rse/plugins/net.izhtw.rse.subsystems.processes.emulator.local/icons/full/obj16/processsubsystem_obj.gif
    	new file:   rse/plugins/net.izhtw.rse.subsystems.processes.emulator.local/icons/full/obj16/processsubsystemlive_obj.gif
    	modified:   rse/plugins/net.izhtw.rse.subsystems.processes.emulator.local/plugin.xml
    	new file:   rse/plugins/net.izhtw.rse.subsystems.processes.emulator.local/src/net/izhtw/rse/subsystems/processes/emulator/local/subsystem/EmulatorProcessServiceSubSystem.java
    	modified:   rse/plugins/net.izhtw.rse.subsystems.processes.emulator.local/src/net/izhtw/rse/subsystems/processes/emulator/local/subsystem/EmulatorProcessSubSystemConfiguration.java

---
+++ rse/plugins/net.izhtw.rse.subsystems.processes.emulator.local/src/net/izhtw/rse/subsystems/processes/emulator/local/subsystem/EmulatorProcessServiceSubSystem.java
@@ -0,0 +1,53 @@
+package net.izhtw.rse.subsystems.processes.emulator.local.subsystem;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.rse.core.model.IHost;
+import org.eclipse.rse.core.subsystems.IConnectorService;
+import org.eclipse.rse.services.clientserver.messages.SystemMessageException;
+import org.eclipse.rse.services.clientserver.processes.IHostProcessFilter;
+import org.eclipse.rse.services.processes.IProcessService;
+import org.eclipse.rse.subsystems.processes.core.subsystem.IHostProcessToRemoteProcessAdapter;
+import org.eclipse.rse.subsystems.processes.core.subsystem.IRemoteProcess;
+import org.eclipse.rse.subsystems.processes.core.subsystem.IRemoteProcessContext;
+import org.eclipse.rse.subsystems.processes.servicesubsystem.IProcessServiceSubSystem;
+import org.eclipse.rse.subsystems.processes.servicesubsystem.ProcessServiceSubSystem;
+
+public class EmulatorProcessServiceSubSystem extends ProcessServiceSubSystem implements IProcessServiceSubSystem{
+
+	public EmulatorProcessServiceSubSystem(IHost host,
+			IConnectorService connectorService,
+			IProcessService hostProcessService,
+			IHostProcessToRemoteProcessAdapter adapter) {
+		super(host, connectorService, hostProcessService, adapter);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean kill(IRemoteProcess process, String signal)
+			throws SystemMessageException {
+		// TODO Auto-generated method stub
+		return super.kill(process, signal);
+	}
+
+	@Override
+	public IRemoteProcess[] listAllProcesses(IHostProcessFilter processFilter,
+			IRemoteProcessContext context, IProgressMonitor monitor)
+			throws InterruptedException, SystemMessageException {
+		// TODO Auto-generated method stub
+		return super.listAllProcesses(processFilter, context, monitor);
+	}
+
+	@Override
+	public void initializeSubSystem(IProgressMonitor monitor)
+			throws SystemMessageException {
+		// TODO Auto-generated method stub
+		super.initializeSubSystem(monitor);
+	}
+
+	@Override
+	public void uninitializeSubSystem(IProgressMonitor monitor) {
+		// TODO Auto-generated method stub
+		super.uninitializeSubSystem(monitor);
+	}
+
+}
--- rse/plugins/net.izhtw.rse.subsystems.processes.emulator.local/src/net/izhtw/rse/subsystems/processes/emulator/local/subsystem/EmulatorProcessSubSystemConfiguration.java
+++ rse/plugins/net.izhtw.rse.subsystems.processes.emulator.local/src/net/izhtw/rse/subsystems/processes/emulator/local/subsystem/EmulatorProcessSubSystemConfiguration.java
@@ -1,7 +1,22 @@
 package net.izhtw.rse.subsystems.processes.emulator.local.subsystem;
 
+import org.eclipse.rse.core.model.IHost;
+import org.eclipse.rse.core.subsystems.ISubSystem;
+import org.eclipse.rse.internal.connectorservice.local.LocalConnectorService;
 import org.eclipse.rse.subsystems.processes.local.LocalProcessSubSystemConfiguration;
+import org.eclipse.rse.subsystems.processes.servicesubsystem.ProcessServiceSubSystem;
 
 public class EmulatorProcessSubSystemConfiguration extends LocalProcessSubSystemConfiguration {
+
+	@Override
+	public ISubSystem createSubSystemInternal(IHost host) {
+		
+		LocalConnectorService connectorService = (LocalConnectorService)getConnectorService(host);
+		
+		ISubSystem subsys = new EmulatorProcessServiceSubSystem(host, connectorService, getProcessService(host), getHostProcessAdapter());
+		
+		return subsys;
+		
+	}
 
 }
